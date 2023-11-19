402e451cd49ba436bfd56ffada0e4a6bd5b54fb0
김승빈/GSB0203 <128358984+GSB0203@users.noreply.github.com>
2023-11-19 19:18:52 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(n > 0) {
+            n -= 7;
+            answer += 1;
+        }
+
+        return answer;
+    }
+}
